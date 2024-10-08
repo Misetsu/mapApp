1bd7180aef48305485fed442daa30f8a681316ad
syu-to <kd1333767@st.kobedenshi.ac.jp>
2024-10-08 15:38:44 +0900

    editのコメント終了

--- app/(camera)/edit.jsx
+++ app/(camera)/edit.jsx
@@ -13,106 +13,108 @@ import firestore from "@react-native-firebase/firestore";
 import FirebaseAuth from "@react-native-firebase/auth";
 
 //画像のサイズを固定
-const { width } = Dimensions.get("window");
+const { width } = Dimensions.get("window"); //画面サイズを取得
 const imageWidth = width * 0.75; // 画面幅の75%
 const imageHeight = (imageWidth * 4) / 3; // 3:4のアスペクト比を維持
 const auth = FirebaseAuth();
 
 export default function test() {
   const [text, setText] = useState(""); // テキスト入力を保持するための状態
-  const [post, setPost] = useState("");
+  const [post, setPost] = useState(""); //場所の名前を保持する
 
-  const reference = storage();
-  const router = useRouter();
-  const params = useLocalSearchParams();
-  const { imageUri, latitude, longitude, spotId } = params;
+  const reference = storage();    //storage()関数を呼び出して、Firebaseストレージのインスタンスを取得
+  //storage()関数はFirebaseのストレージサービスにアクセスするための関数
+  const router = useRouter();     //ルーターにアクセスするための変数
+  const params = useLocalSearchParams();  //別のページから送られた値が入る。
+  const { imageUri, latitude, longitude, spotId } = params; //別ページから送られた値が入る。
 
   const uploadPost = async () => {
     // 写真をstorageに格納
-    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    const randomNumber = Math.floor(Math.random() * 100) + 1;   //1～100の数字をランダムに生成する
     const imagePath =
-      "photo/image-" + new Date().getTime().toString() + randomNumber;
-    await reference.ref(imagePath).putFile(imageUri);
+      "photo/image-" + new Date().getTime().toString() + randomNumber;  //現在の時間とさっきのランダムな数字を合わせて画像ファイルを作成
+    await reference.ref(imagePath).putFile(imageUri);//ファイルをアップロード
     console.log(auth.currentUser.uid);
 
     // メイン画面の投稿であれば、現在地のスポットを追加
-    if (spotId == 0) {
+    if (spotId == 0) {  //スポットidが0であれば
       const querySnapshot = await firestore()
-        .collection("spot")
-        .orderBy("id", "desc")
-        .get();
+        .collection("spot") //spotテーブルから
+        .orderBy("id", "desc")  //idを降順に並び替えて
+        .get(); //取り出す
 
-      const maxId = querySnapshot.docs[0].data().id + 1;
+      const maxId = querySnapshot.docs[0].data().id + 1;  //降順に並び替えて取得したスポットidの0番地目、つまり一番でけぇスポットidのプラス１を取得
 
-      firestore().collection("spot").add({
-        id: maxId,
-        mapLatitude: latitude,
-        mapLongitude: longitude,
-        name: text,
-        areaRadius: 50,
+      firestore().collection("spot").add({  //新しいスポットの追加
+        id: maxId,  //idをさっき作成したでけぇidにプラス1した値にする
+        mapLatitude: latitude,  //緯度
+        mapLongitude: longitude,  //経度
+        name: text, //テキスト
+        areaRadius: 50, //範囲
       });
 
       const queryPost = await firestore()
-        .collection("post")
-        .orderBy("id", "desc")
+        .collection("post")   //postテーブルから
+        .orderBy("id", "desc")  //idを降順に
         .get();
 
-      const maxPostId = queryPost.docs[0].data().id + 1;
+      const maxPostId = queryPost.docs[0].data().id + 1;  //最新投稿のidを作成
 
       firestore()
-        .collection("photo")
+        .collection("photo")  //photoテーブルに追加
         .add({
-          imagePath: imagePath,
-          postId: maxPostId,
-          spotId: maxId,
-          userId: auth.currentUser.uid,
+          imagePath: imagePath, //画像パス
+          postId: maxPostId,  //さっきのid
+          spotId: maxId,  //新しく作成したスポット
+          userId: auth.currentUser.uid, //投稿ユーザー
         })
-        .then()
-        .catch((error) => console.log(error));
+        .then() //追加終了合図
+        .catch((error) => console.log(error));  //エラー
 
       firestore()
-        .collection("post")
+        .collection("post") //postテーブルに追加
         .add({
-          id: maxId,
-          postTxt: post,
-          spotId: maxId,
-          userId: auth.currentUser.uid,
+          id: maxId,  //さっきと同じid
+          postTxt: post,  //投稿内容
+          spotId: maxId,  //最新スポット
+          userId: auth.currentUser.uid, //ユーザーid
         })
-        .then()
-        .catch((error) => console.log(error));
-    } else {
+        .then() //終了合図
+        .catch((error) => console.log(error));  //エラー
+    } else {  //spotidが0でない(既存スポットへの投稿)
       console.log(imagePath, spotId);
       firestore()
-        .collection("photo")
+        .collection("photo")  //photoテーブルに追加
         .add({
-          imagePath: imagePath,
-          spotId: parseInt(spotId),
-          userId: auth.currentUser.uid,
+          imagePath: imagePath, //イメージパス
+          spotId: parseInt(spotId), //int型に変換して受け取ったスポットid
+          userId: auth.currentUser.uid, //ユーザーid
         })
-        .then()
-        .catch((error) => console.log(error));
+        .then() //終了合図
+        .catch((error) => console.log(error));  //エラー
 
       firestore()
-        .collection("post")
+        .collection("post") //postテーブルに追加
         .add({
-          postTxt: post,
-          spotId: parseInt(spotId),
-          userId: auth.currentUser.uid,
+          postTxt: post,  //投稿
+          spotId: parseInt(spotId), //スポットid
+          userId: auth.currentUser.uid, //ユーザーid
         })
-        .then()
-        .catch((error) => console.log(error));
+        .then() //おわり
+        .catch((error) => console.log(error));    //えら
     }
 
-    router.replace({ pathname: "/" });
+    router.replace({ pathname: "/" });  //routerのreplace メソッドにを使うと戻るボタンを押したときにこのページに戻ってくることはなくなる
+                                        // "/"はホーム画面を表す
   };
 
   return (
-    <View style={{ flex: 1 }}>
-      <Image source={{ uri: imageUri }} style={styles.imageContainer} />
-      {spotId == 0 ? (
+    <View style={{ flex: 1 }} /*すべての親コンポーネント*/>
+      <Image source={{ uri: imageUri }} style={styles.imageContainer} /*さっき撮影された画像を表示*//>
+      {spotId == 0 ? (  //スポットidが0(新規スポットへ投稿)なら
         <TextInput
-          style={styles.textbox}
-          placeholder="場所の名前を入力"
+          style={styles.textbox}  
+          placeholder="場所の名前を入力"  //スポットの名前を入力させる
           maxLength={30} // 文字数制限を30文字に設定
           onChangeText={setText}
           value={text}
@@ -122,11 +124,11 @@ export default function test() {
       )}
       <TextInput
         style={styles.textbox}
-        placeholder="投稿の文章を入力"
+        placeholder="投稿の文章を入力"  //投稿の文章
         onChangeText={setPost}
         value={post}
       />
-      <Pressable onPress={uploadPost} style={styles.uploadButton} />
+      <Pressable onPress={uploadPost} style={styles.uploadButton}/*投稿をアップロードする */ />
     </View>
   );
 }
