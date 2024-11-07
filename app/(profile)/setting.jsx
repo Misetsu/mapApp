bf4f103445aab6910cc6c6fa5d6e25f4b5b37860
KAZ02 <kd1339181@st.kobedenshi.ac.jp>
2024-11-07 13:10:37 +0900

    色変更

--- app/(profile)/setting.jsx
+++ app/(profile)/setting.jsx
@@ -17,7 +17,9 @@ export default function myPage() {
   const router = useRouter();
 
   const handleBackPress = () => {
-    router.back(); // 前の画面に戻る
+    if (router) {
+      router.back();
+    }
   };
 
   const navigation = useNavigation();
@@ -30,7 +32,7 @@ export default function myPage() {
   //
 
   return (
-    <ScrollView>
+    <ScrollView style={styles.scrview}>
       <TouchableOpacity
         onPress={handleBackPress}
         style={{
@@ -45,7 +47,7 @@ export default function myPage() {
       </TouchableOpacity>
 
       <View style={styles.container}>
-        <Text style={styles.pagetitle}>SETTING</Text>
+        <Text style={styles.pagetitle}>設定</Text>
 
         <TouchableOpacity
           style={styles.button}
@@ -77,34 +79,39 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 20,
   },
+  scrview: {
+    backgroundColor: "#F2F5C8",
+  },
   profileContainer: {
     alignItems: "center",
   },
   button: {
     justifyContent: "center", // 画像をボタンの垂直方向の中央に揃える
     alignItems: "center", // 画像をボタンの水平方向の中央に揃える
-    backgroundColor: "#F2F2F2",
+    backgroundColor: "#A3DE83",
     height: 50,
-    marginTop: 10, // ボタン間にスペースを追加
-  },
-  closeButton: {
-    justifyContent: "center", // 画像をボタンの垂直方向の中央に揃える
-    alignItems: "center", // 画像をボタンの水平方向の中央に揃える
-    backgroundColor: "#F2F2F2",
-    paddingHorizontal: 20,
-    paddingVertical: 5,
+    margin: 10, // ボタン間にスペースを追加
   },
   buttonText: {
     fontSize: 18,
-    color: "black",
+    color: "#000000",
     textAlign: "center",
     fontWeight: "300",
   },
   pagetitle: {
     fontSize: 30,
-    marginBottom: 10,
+    height: 70,
+    marginTop: 0,
     textAlign: "center",
     fontWeight: "300",
+    color: "#000000",
+  },
+  subtitle: {
+    fontSize: 18,
+    margin: 10,
+    textAlign: "center",
+    fontWeight: "600",
+    color: "#000000",
   },
   displayName: {
     fontSize: 15,
@@ -125,33 +132,29 @@ const styles = StyleSheet.create({
     borderRadius: 50,
     backgroundColor: "#ccc",
   },
-  followList: {
-    display: "flex",
-    flexDirection: "row",
-    gap: 10,
-    margin: 10,
-  },
-  listProfileImage: {
-    width: 30,
-    height: 30,
-    borderRadius: 15,
-  },
   FFtext: {
     fontSize: 16,
-    fontWeight: "600",
+    fontWeight: "900",
     textAlign: "center",
   },
   FFcontainer: {
+    flexDirection: "row", // 子要素を横並びに配置
+    justifyContent: "space-between", // 子要素間にスペースを空ける
+    alignItems: "center", // 垂直方向の中央に揃える
+    width: "80%", // 横幅を80%に設定（任意）
+    padding: 5, // 全体にパディング
+  },
+  FFnum: {
+    padding: 10,
+  },
+  ChangeStatus: {
     justifyContent: "space-between",
     flexDirection: "row", // 子要素を横並びに配置
     alignItems: "center", // 垂直方向の中央に揃える
-    padding: 5, // 全体にパディング
-    height: 50,
-    marginBottom: 10, // ボタン間にスペースを追加
-  },
-  SwitchBtn: {},
-  FFnum: {
-    padding: 10,
+    width: "90%", // 横幅を80%に設定（任意）
+    alignSelf: "center",
+    margin: 10,
+    backgroundColor: "#F2F5C8",
   },
   modalOverlay: {
     flex: 1,
@@ -160,89 +163,56 @@ const styles = StyleSheet.create({
     backgroundColor: "rgba(0, 0, 0, 0.5)", // 背景を半透明に
   },
   modalContent: {
-    width: 300,
+    width: "90%",
     padding: 20,
-    backgroundColor: "white",
+    paddingTop: 15,
+    backgroundColor: "#F2F5C2",
     borderRadius: 10,
+  },
+  listUsernamecontainer: {
+    justifyContent: "center",
     alignItems: "center",
+    marginLeft: 10,
+  },
+  listUsername: {
+    fontSize: 18,
+    color: "black",
+    fontWeight: "300",
+  },
+  followListuser: {
+    display: "flex",
+    flexDirection: "row",
+    margin: 10,
+    width: "100%",
+  },
+  listProfileImage: {
+    width: 50,
+    height: 50,
+    borderRadius: 30,
   },
   textInput: {
     margin: 5,
+    marginTop: 0,
     marginBottom: 0,
     fontSize: 20,
     height: 40,
-    borderBottomWidth: 2,
+    borderBottomWidth: 3,
+    borderColor: "#239D60",
     marginVertical: 16,
     color: "black",
     fontWeight: "300",
   },
-  submit: {
+  backButton: {
     justifyContent: "center", // 画像をボタンの垂直方向の中央に揃える
     alignItems: "center", // 画像をボタンの水平方向の中央に揃える
-    backgroundColor: "black",
-    height: 50,
-    marginTop: 10, // ボタン間にスペースを追加
+    backgroundColor: "#F2F5C8",
+    width: 70,
+    height: 70,
+    marginTop: 5, // ボタン間にスペースを追加
   },
-  submitText: {
-    fontSize: 18,
-    fontWeight: "600",
-    color: "#f2f2f2",
-    textAlign: "center",
-  },
-  linklabel: {
-    fontSize: 16,
-    paddingTop: 15,
-    paddingBottom: 15,
-    textAlign: "center",
-    textDecorationLine: "underline",
-    color: "#1a0dab",
-    fontWeight: "600",
-  },
-  backButton: {
-    justifyContent: "center",
-    alignItems: "center",
-    height: 50,
-  },
-  backButtonText: {
-    fontSize: 18,
-    color: "black",
-    textAlign: "center",
-    fontWeight: "300",
-  },
-  //
-  //
-  //
-  container: {
-    flex: 1,
-    padding: 20,
-  },
-  track: {
-    width: 300,
-    height: 50,
-    backgroundColor: "#ddd",
-    borderRadius: 25,
-    justifyContent: "center",
-    padding: 5,
-    position: "relative",
-  },
-  slider: {
-    width: 50,
-    height: 40,
-    backgroundColor: "#ff6347",
-    borderRadius: 20,
-    justifyContent: "center",
-    alignItems: "center",
+  Back: {
     position: "absolute",
-  },
-  sliderText: {
-    color: "#fff",
-    fontSize: 20,
-  },
-  slideBtn: {
-    position: "absolute",
-    left: "50%",
-    transform: [{ translateX: -75 }],
-    color: "#333",
-    fontSize: 16,
+    top: 0,
+    left: 0,
   },
 });
