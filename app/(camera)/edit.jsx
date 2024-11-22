902bf99f73b13f9283a93326411677c98ecfcf61
Misetsu <nakitamak@gmail.com>
2024-11-22 11:36:20 +0900

    文言修正

--- app/(camera)/edit.jsx
+++ app/(camera)/edit.jsx
@@ -17,6 +17,7 @@ import { useLocalSearchParams, useRouter } from "expo-router";
 import storage from "@react-native-firebase/storage";
 import firestore from "@react-native-firebase/firestore";
 import FirebaseAuth from "@react-native-firebase/auth";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const { width } = Dimensions.get("window");
 const imageWidth = width * 0.75;
@@ -41,11 +42,7 @@ export default function edit() {
     // アラート表示
     Alert.alert(
       "投稿に関する注意事項",
-      `- 投稿は自己責任で行ってください。\n
-      - 他人が不快になる内容や、法令・権利を侵害する内容は禁止です。\n
-      - 人物が映る写真は、事前に本人の了承を得てください。\n
-      - 投稿内容は管理者確認後に掲載される場合があります。\n
-      管理者が不適切と判断した場合、投稿は掲載されない場合があります。`,
+      `- 投稿は自己責任で行ってください。\n- 他人が不快になる内容や、法令・権利を侵害する内容は禁止です。\n- 人物が映る写真は、事前に本人の了承を得てください。\n- 投稿内容は管理者確認後に掲載される場合があります。\n管理者が不適切と判断した場合、投稿は掲載されない場合があります。`,
       [
         {
           text: "今後表示しない",
@@ -72,7 +69,6 @@ export default function edit() {
     }
   }, [showAlert]);
 
-  
   const [text, setText] = useState("");
   const [post, setPost] = useState("");
   const [focusedInput, setFocusedInput] = useState(null);
@@ -149,11 +145,14 @@ export default function edit() {
         })
         .catch((error) => console.log(error));
 
-      await firestore().collection("users").doc(auth.currentUser.uid).update({
-        spotCreate: spotNo,
-        spotPoint: parseInt(point),
-        lastPostAt: currentTime,
-      });
+      await firestore()
+        .collection("users")
+        .doc(auth.currentUser.uid)
+        .update({
+          spotCreate: spotNo,
+          spotPoint: parseInt(point),
+          lastPostAt: currentTime,
+        });
 
       handleVisitState(maxId);
     } else {
