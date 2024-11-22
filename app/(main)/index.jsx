37f1841f0dca1af5fdddeadf29b545140c8a5d95
syu-to <kd1333767@st.kobedenshi.ac.jp>
2024-11-22 09:51:33 +0900

    バグ修正

--- app/(main)/index.jsx
+++ app/(main)/index.jsx
@@ -791,7 +791,7 @@ export default function TrackUserMapView() {
         setMarkerCords(fetchResult);
       }
     } else {
-      setmapflag(true);
+      setmapflag(false);
       setMarkerCords([]);
     }
 
