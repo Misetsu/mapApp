281ffe84ceaa0a4a9bdade7a8e465b2872b3c9bb
syu-to <kd1333767@st.kobedenshi.ac.jp>
2025-03-01 01:27:42 +0900

    auth

---
+++ app/(auth)/loginForm.jsx
@@ -0,0 +1,293 @@
+import React, { useState } from "react";
+import {
+  ScrollView,
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  Image,
+  StyleSheet,
+  Alert,
+} from "react-native";
+import { useRouter } from "expo-router";
+import FirebaseAuth from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import Toast from 'react-native-simple-toast';
+
+const auth = FirebaseAuth();
+
+GoogleSignin.configure({
+  webClientId:
+    "224298539879-t62hp3sk9t27ecupcds9d8aj29jr9hmm.apps.googleusercontent.com",
+});
+
+export default function LoginScreen() {
+  const router = useRouter();
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  const handleBackPress = () => {
+    router.back(); // 前の画面に戻る
+  };
+
+  const navigateSignup = () => {
+    router.push("/signupForm");
+  };
+
+  const signInWithGoogle = async () => {
+    // Google のログイン画面を表示して認証用の ID トークンを取得する
+    try {
+      const user = await GoogleSignin.signIn();
+      const idToken = user.idToken;
+      if (idToken === null) {
+        return;
+      }
+
+      // 取得した認証情報 (ID トークン) を元にサインインする
+      const credential = FirebaseAuth.GoogleAuthProvider.credential(idToken);
+      await auth.signInWithCredential(credential);
+
+      const querySnapshot = await firestore()
+        .collection("users")
+        .where("uid", "==", auth.currentUser.uid) // 特定の条件を指定
+        .get();
+        Toast.show("ログインしました");
+      if (querySnapshot.empty) {
+        router.push({
+          pathname: "/signupFormGoogle",
+          params: {
+            uid: auth.currentUser.uid,
+            displayName: auth.currentUser.displayName,
+            photoURL: auth.currentUser.photoURL,
+          },
+        });
+      } else {
+        router.replace({ pathname: "/" });
+      }
+    } catch (error) {
+      Alert.alert("ログインに失敗しました。", error.code);
+    }
+  };
+
+  const signInWithEmail = async () => {
+    try {
+      if (userEmail != "" && userPassword != "") {
+        await auth
+          .signInWithEmailAndPassword(userEmail, userPassword)
+          .then(() => {
+            router.replace({ pathname: "/" });
+            Toast.show("ログインしました");
+          })
+          .catch((error) => {
+            console.log(error);
+            Alert.alert(
+              "ログインに失敗しました。",
+              "入力内容を再確認してください。"
+            );
+          });
+      } else {
+        Alert.alert("ログインに失敗しました。", "入力してください。");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleChangePassword = async () => {
+    const userSnapshot = await firestore()
+      .collection("users")
+      .where("email", "==", userEmail)
+      .get();
+
+    if (!userSnapshot.empty) {
+      auth
+        .sendPasswordResetEmail(userEmail)
+        .then(() => {
+          alert(
+            "パスワードを変更するメールを入力されたメールアドレスに送信しました。"
+          );
+        })
+        .catch((error) => {
+          alert(error);
+        });
+    } else {
+      alert("入力されたメールアドレスが登録されていません。");
+    }
+  };
+
+  return (
+    <ScrollView style={styles.scrview}>
+      <View style={styles.container}>
+        <Text style={styles.pagetitle}>ログイン</Text>
+        <Text style={styles.displayName}>メールアドレス</Text>
+        <TextInput
+          style={styles.textInput}
+          value={userEmail}
+          onChangeText={setUserEmail}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          placeholder="メールアドレス"
+        />
+
+        <Text style={styles.displayName}>パスワード</Text>
+        <TextInput
+          style={styles.textInput}
+          value={userPassword}
+          onChangeText={setUserPassword}
+          secureTextEntry
+          placeholder="パスワード"
+        />
+
+        <TouchableOpacity style={styles.submit} onPress={signInWithEmail}>
+          <Text style={styles.submitText}>ログイン</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.Googlebutton}
+          onPress={signInWithGoogle}
+        >
+          <Image
+            source={require("./../image/android_light_rd_SI.png")}
+            style={styles.imageButton}
+          />
+        </TouchableOpacity>
+        <View
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <TouchableOpacity onPress={handleChangePassword}>
+            <Text style={styles.linklabel}>パスワードを忘れた場合</Text>
+          </TouchableOpacity>
+        </View>
+        <Text style={styles.noamllabel}>━━━ はじめての方はこちら ━━━</Text>
+
+        <TouchableOpacity style={styles.button} onPress={navigateSignup}>
+          <Text style={styles.buttonText}>新規アカウント作成</Text>
+        </TouchableOpacity>
+        <View style={styles.Back}>
+          <TouchableOpacity style={styles.backButton} onPress={handleBackPress}>
+            <Image
+              source={require("./../image/Left_arrow.png")}
+              style={styles.actionButton}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+  },
+  scrview: {
+    backgroundColor: "#F2F5C8",
+  },
+  pagetitle: {
+    fontSize: 24,
+    height: 30,
+    marginBottom: 10,
+    textAlign: "center",
+    fontWeight: "300",
+    color: "#000000",
+  },
+  displayName: {
+    fontSize: 15,
+    marginTop: 10,
+    marginLeft: 10,
+    textAlign: "left",
+    alignItems: "flex-start",
+    fontWeight: "300",
+  },
+  textInput: {
+    margin: 5,
+    marginTop: 0,
+    marginBottom: 0,
+    fontSize: 20,
+    height: 40,
+    borderBottomWidth: 3,
+    borderColor: "#239D60",
+    marginVertical: 16,
+    color: "black",
+    fontWeight: "300",
+  },
+  noamllabel: {
+    fontSize: 15,
+    margin: 10,
+    textAlign: "center",
+    fontWeight: "600",
+    color: "#239D60",
+  },
+  linklabel: {
+    fontSize: 15,
+    margin: 10,
+    width: "70%",
+    textAlign: "center",
+    textDecorationLine: "underline",
+    color: "#1a0dab",
+    fontWeight: "600",
+  },
+  button: {
+    justifyContent: "center", // 画像をボタンの垂直方向の中央に揃える
+    alignItems: "center", // 画像をボタンの水平方向の中央に揃える
+    backgroundColor: "#A3DE83",
+    height: 50,
+    margin: 10, // ボタン間にスペースを追加
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#000000",
+    textAlign: "center",
+    fontWeight: "300",
+  },
+  Googlebutton: {
+    justifyContent: "center", // 画像をボタンの垂直方向の中央に揃える
+    alignItems: "center", // 画像をボタンの水平方向の中央に揃える
+    backgroundColor: "#F2F5C8",
+    height: 50,
+    margin: 10,
+  },
+  submit: {
+    justifyContent: "center", // 画像をボタンの垂直方向の中央に揃える
+    alignItems: "center", // 画像をボタンの水平方向の中央に揃える
+    backgroundColor: "#239D60",
+    height: 50,
+    margin: 10, // ボタン間にスペースを追加
+  },
+  submitText: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#f2f2f2",
+    textAlign: "center",
+  },
+  imageButton: {
+    justifyContent: "center", // 画像をボタンの垂直方向の中央に揃える
+    alignItems: "center", // 画像をボタンの水平方向の中央に揃える
+    height: 50, // 画像のサイズを指定
+    width: 300,
+    resizeMode: "contain", // 画像のリサイズ方法を指定
+  },
+  actionButton: {
+    width: 30,
+    height: 30,
+    padding: 5,
+    margin: 5,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center", // ボタン内のテキストを中央に配置
+    alignItems: "center",
+  },
+  backButton: {
+    justifyContent: "center", // 画像をボタンの垂直方向の中央に揃える
+    alignItems: "center", // 画像をボタンの水平方向の中央に揃える
+    width: 70,
+    height: 70,
+    marginTop: 3, // ボタン間にスペースを追加
+  },
+  Back: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+  },
+});
---
+++ app/(auth)/signupForm.jsx
@@ -0,0 +1,356 @@
+import React, { useState } from "react";
+import {
+  ScrollView,
+  View,
+  TouchableOpacity,
+  Text,
+  TextInput,
+  StyleSheet,
+  Alert,
+  Image,
+  Linking,
+} from "react-native";
+import { useRouter } from "expo-router";
+import FirebaseAuth from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
+import CheckBox from "@react-native-community/checkbox";
+
+const auth = FirebaseAuth();
+
+export default function SignupScreen() {
+  const router = useRouter();
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+  const [userPasswordVal, setUserPasswordVal] = useState("");
+  const [userName, setUserName] = useState("");
+  const [isChecked, setIsChecked] = useState(false);
+
+  const handleBackPress = () => {
+    router.back(); // 前の画面に戻る
+  };
+
+  const navigateLogin = () => {
+    router.push("/loginForm");
+  };
+
+  const validateEmail = (email) => {
+    return email.match(
+      /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+  };
+
+  const validatePassword = (password) => {
+    if (password.match(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/)) {
+      if (password == userPasswordVal) {
+        return true;
+      } else {
+        return false;
+      }
+    } else {
+      return false;
+    }
+  };
+
+  const signUpWithEmail = async () => {
+    if (validateEmail(userEmail)) {
+      if (validatePassword(userPassword)) {
+        try {
+          await auth.createUserWithEmailAndPassword(userEmail, userPassword);
+
+          const credential = await auth.signInWithEmailAndPassword(
+            userEmail,
+            userPassword
+          );
+
+          const update = {
+            displayName: userName,
+          };
+
+          await auth.currentUser.updateProfile(update);
+
+          firestore()
+            .collection("users")
+            .doc(auth.currentUser.uid)
+            .set({
+              uid: auth.currentUser.uid,
+              displayName: auth.currentUser.displayName,
+              email: auth.currentUser.email,
+              lastPostAt: "0", // TODO
+              publicStatus: 0, // TODO
+              spotCreate: 0,
+              spotPoint: 0,
+              photoURL:
+                "https://firebasestorage.googleapis.com/v0/b/mapapp-96457.appspot.com/o/profile%2Fphoto17256005513463?alt=media&token=847894f6-3cb5-46c5-833e-91e30bc3ede8",
+            })
+            .then()
+            .catch((error) => console.log(error));
+
+          firestore().collection("star").doc(auth.currentUser.uid).set({});
+
+          router.replace({ pathname: "/" });
+        } catch (error) {
+          console.error("Error signing up:", error);
+        }
+      } else {
+        Alert.alert("", "パスワードを正しく入力してください。");
+      }
+    } else {
+      Alert.alert("", "メールアドレスを正しく入力してください。");
+    }
+  };
+
+  return (
+    <ScrollView style={styles.scrview}>
+      <View style={styles.container}>
+        <Text style={styles.pagetitle}>アカウント作成</Text>
+
+        <Text style={styles.displayName}>メールアドレス</Text>
+        <TextInput
+          style={styles.textInput}
+          value={userEmail}
+          onChangeText={setUserEmail}
+          keyboardType="email-address"
+          autoCapitalize="none"
+          placeholder="メールアドレス"
+        />
+        <Text style={styles.noamllabel}>
+          有効なメールアドレスを入力してください
+        </Text>
+        <Text style={styles.displayName}>ユーザー名</Text>
+        <TextInput
+          style={styles.textInput}
+          value={userName}
+          onChangeText={setUserName}
+          maxLength={16}
+          placeholder="ユーザー名"
+        />
+        <Text style={styles.noamllabel}>
+          ユーザー名{"(名前)"}を入力してください
+          半角英文字、半角数字、全角文字しか含まれられません
+        </Text>
+        <Text style={styles.displayName}>パスワード</Text>
+        <TextInput
+          style={styles.textInput}
+          value={userPassword}
+          onChangeText={setUserPassword}
+          secureTextEntry
+          placeholder="パスワード"
+        />
+        <Text style={styles.noamllabel}>
+          8文字以上、半角英大文字、半角英小文字、半角数字を含むパスワードを入力してください
+        </Text>
+        <Text style={styles.displayName}>確認用パスワード</Text>
+        <TextInput
+          style={styles.textInput}
+          value={userPasswordVal}
+          onChangeText={setUserPasswordVal}
+          secureTextEntry
+          placeholder="確認用パスワード"
+        />
+        <Text style={styles.noamllabel}>
+          もう一度同じパスワードを入力してください
+        </Text>
+
+        <View style={styles.checkboxContainer}>
+          <CheckBox
+            value={isChecked}
+            onValueChange={setIsChecked}
+            tintColors={{ true: "#239D60", false: "#239D60" }} // チェックボックスの色
+            style={styles.checkbox} // サイズ変更用のスタイル
+          />
+          <Text style={styles.noamllabel}>
+            <TouchableOpacity
+              onPress={() => {
+                Linking.openURL(
+                  "https://ekatiihs.wordpress.com/%E5%88%A9%E7%94%A8%E8%A6%8F%E7%B4%84/"
+                );
+              }}
+            >
+              <Text
+                style={[styles.plainlabel, { textDecorationLine: "underline" }]}
+              >
+                利用規約
+              </Text>
+            </TouchableOpacity>
+            、
+            <TouchableOpacity
+              onPress={() => {
+                Linking.openURL(
+                  "https://ekatiihs.wordpress.com/%E3%83%97%E3%83%A9%E3%82%A4%E3%83%90%E3%82%B7%E3%83%BC%E3%83%9D%E3%83%AA%E3%82%B7%E3%83%BC/"
+                );
+              }}
+            >
+              <Text
+                style={[styles.plainlabel, { textDecorationLine: "underline" }]}
+              >
+                プライバシーポリシー
+              </Text>
+            </TouchableOpacity>
+            {"\n"}
+            をすべて読み、同意します
+          </Text>
+        </View>
+        <TouchableOpacity
+          style={[styles.submit, !isChecked && styles.disabledButton]}
+          onPress={signUpWithEmail}
+          disabled={!isChecked}
+        >
+          <Text style={styles.submitText}>アカウント作成</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.noamllabel}>
+          ━ アカウントをお持ちの方はこちら ━
+        </Text>
+
+        <TouchableOpacity style={styles.button} onPress={navigateLogin}>
+          <Text style={styles.buttonText}>ログイン</Text>
+        </TouchableOpacity>
+
+        <View style={styles.Back}>
+          <TouchableOpacity style={styles.backButton} onPress={handleBackPress}>
+            <Image
+              source={require("./../image/Left_arrow.png")}
+              style={styles.actionButton}
+            />
+          </TouchableOpacity>
+        </View>
+      </View>
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+  },
+  scrview: {
+    flex: 1,
+    backgroundColor: "#F2F5C8",
+  },
+  pagetitle: {
+    fontSize: 24,
+    height: 30,
+    marginBottom: 10,
+    textAlign: "center",
+    fontWeight: "300",
+    color: "#000000",
+  },
+  displayName: {
+    fontSize: 18,
+    marginTop: 15,
+    marginLeft: 10,
+    textAlign: "left",
+    alignItems: "flex-start",
+    fontWeight: "600",
+    color: "#239D60",
+  },
+  textInput: {
+    margin: 5,
+    marginTop: 0,
+    marginBottom: 0,
+    fontSize: 16,
+    height: 40,
+    borderBottomWidth: 3,
+    borderColor: "#239D60",
+    marginVertical: 16,
+    color: "black",
+    fontWeight: "300",
+  },
+  noamllabel: {
+    fontSize: 15,
+    margin: 5,
+    fontWeight: "600",
+    color: "#239D60",
+  },
+  plainlabel: {
+    fontSize: 15,
+    fontWeight: "600",
+    color: "#239D60",
+  },
+  linklabel: {
+    fontSize: 15,
+    margin: 10,
+    width: "70%",
+    textAlign: "center",
+    textDecorationLine: "underline",
+    color: "#1a0dab",
+    fontWeight: "600",
+  },
+  button: {
+    justifyContent: "center", // 画像をボタンの垂直方向の中央に揃える
+    alignItems: "center", // 画像をボタンの水平方向の中央に揃える
+    backgroundColor: "#A3DE83",
+    height: 50,
+    margin: 10, // ボタン間にスペースを追加
+  },
+  buttonText: {
+    fontSize: 18,
+    color: "#000000",
+    textAlign: "center",
+    fontWeight: "300",
+  },
+  Googlebutton: {
+    justifyContent: "center", // 画像をボタンの垂直方向の中央に揃える
+    alignItems: "center", // 画像をボタンの水平方向の中央に揃える
+    backgroundColor: "#F2F5C8",
+    height: 50,
+    margin: 10,
+  },
+  submit: {
+    justifyContent: "center", // 画像をボタンの垂直方向の中央に揃える
+    alignItems: "center", // 画像をボタンの水平方向の中央に揃える
+    backgroundColor: "#239D60",
+    height: 50,
+    margin: 10, // ボタン間にスペースを追加
+  },
+  submitText: {
+    fontSize: 18,
+    fontWeight: "600",
+    color: "#f2f2f2",
+    textAlign: "center",
+  },
+  imageButton: {
+    justifyContent: "center", // 画像をボタンの垂直方向の中央に揃える
+    alignItems: "center", // 画像をボタンの水平方向の中央に揃える
+    height: 50, // 画像のサイズを指定
+    width: 300,
+    resizeMode: "contain", // 画像のリサイズ方法を指定
+  },
+  actionButton: {
+    width: 30,
+    height: 30,
+    padding: 5,
+    margin: 5,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center", // ボタン内のテキストを中央に配置
+    alignItems: "center",
+  },
+  backButton: {
+    justifyContent: "center", // 画像をボタンの垂直方向の中央に揃える
+    alignItems: "center", // 画像をボタンの水平方向の中央に揃える
+    width: 70,
+    height: 70,
+    marginTop: 3, // ボタン間にスペースを追加
+  },
+  Back: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+  },
+  checkboxContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center", // 中央揃え
+    margin: 10,
+  },
+  checkbox: {
+    width: 30, // サイズを少し大きく
+    height: 30,
+    padding: 10,
+    marginRight: 10,
+  },
+  disabledButton: {
+    backgroundColor: "gray",
+  },
+});
