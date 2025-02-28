44ef53c083118dc537f154e643a52034d497b222
syu-to <kd1333767@st.kobedenshi.ac.jp>
2025-03-01 02:00:43 +0900

    RepliesList&Modal

---
+++ app/component/RepliesList.jsx
@@ -0,0 +1,210 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+} from "react-native";
+import { formatInTimeZone } from "date-fns-tz";
+import firestore from "@react-native-firebase/firestore";
+import ReplieModal from "./repliemodal";
+
+const RepliesList = ({ replies, navigateProfile, postId }) => {
+  const [modalVisible, setmodalVisible] = useState(false);
+  const [replie, setReplies] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const setRepliesModal = (item) => {
+    fetchData(item);
+    setmodalVisible(true);
+  };
+
+  const fetchData = async (item) => {
+    setLoading(true);
+    try {
+      const repliesSnapshot = await firestore()
+        .collection("replies")
+        .where("postId", "==", parseInt(postId))
+        .where("parentReplyId", "==", parseInt(item.parentReplyId))
+        .orderBy("timestamp", "asc")
+        .get();
+
+      const repliesData = await Promise.all(
+        repliesSnapshot.docs.map(async (doc) => {
+          const queryUser = await firestore()
+            .collection("users")
+            .where("uid", "==", doc.data().userId)
+            .get();
+
+          const userData = queryUser.docs[0].data();
+          return {
+            id: doc.id,
+            ...doc.data(),
+            userData,
+          };
+        })
+      );
+      setReplies(repliesData);
+    } catch (error) {
+      console.log(error);
+    }
+    setLoading(false);
+  };
+
+  const renderReply = ({ item }) => (
+    <View
+      style={[
+        styles.replyContainer,
+        item.hantei === 1 && styles.indentedReplyContainer, // hanteiが1なら全体をインデント
+      ]}
+    >
+      <View style={styles.header}>
+        <TouchableOpacity
+          style={styles.userBar}
+          onPress={() => navigateProfile(item.userId)}
+        >
+          <Image
+            source={{ uri: item.userData.photoURL }}
+            style={styles.iconImage}
+          />
+          <Text style={styles.postUser}>{item.userData.displayName}</Text>
+        </TouchableOpacity>
+      </View>
+      <Text style={styles.replyText}>{item.text}</Text>
+      <Text style={styles.replyTimestamp}>
+        {formatInTimeZone(
+          new Date(item.timestamp),
+          "Asia/Tokyo",
+          "yyyy年MM月dd日 HH:mm"
+        )}
+      </Text>
+      {/* 返信ボタン */}
+      {item.hantei === 0 ? (
+        <TouchableOpacity onPress={() => setRepliesModal(item)}>
+          <Text style={styles.replyButton}>返信</Text>
+        </TouchableOpacity>
+      ) : null}
+      {/* 返信入力フィールド */}
+      <ReplieModal
+        visible={modalVisible}
+        items={replie}
+        onClose={() => setmodalVisible(false)}
+        postId={postId}
+        navigateProfile={navigateProfile}
+        parentReplyId={item.parentReplyId}
+        loading={loading}
+      />
+    </View>
+  );
+
+  return (
+    <View style={styles.liststyle}>
+      <FlatList
+        data={replies} // ソートされた配列を使用
+        renderItem={renderReply}
+        keyExtractor={(item) => item.id}
+        style={styles.repliesList}
+        contentContainerStyle={{ flexGrow: 1 }}
+        ListEmptyComponent={
+          <Text style={styles.noRepliesText}>まだ返信がありません。</Text>
+        }
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  replyContainer: {
+    paddingHorizontal: 10,
+    paddingTop: 5,
+    paddingBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "lightgray",
+  },
+  indentedReplyContainer: {
+    marginLeft: 40, // 4マス分のインデント
+  },
+  replyText: {
+    fontSize: 14,
+    paddingHorizontal: 10,
+  },
+  replyTimestamp: {
+    fontSize: 12,
+    color: "gray",
+  },
+  noRepliesText: {
+    textAlign: "center",
+    color: "gray",
+    marginTop: 10,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  userBar: {
+    flexDirection: "row",
+    justifyContent: "center",
+    gap: 5,
+  },
+  postUser: {
+    flexDirection: "row",
+    alignItems: "center",
+    fontSize: 14,
+    color: "#000000",
+    fontWeight: "300",
+    alignSelf: "flex-start", // 子要素の横幅に合わせる
+    padding: 5,
+    paddingLeft: 0,
+  },
+  iconImage: {
+    width: 26,
+    height: 26,
+    borderRadius: 13,
+  },
+  repliesList: {
+    marginTop: 10,
+  },
+  replyButton: {
+    color: "#239D60",
+    fontWeight: "600",
+    paddingTop: 5,
+    paddingHorizontal: 5,
+  },
+  replyInputContainer: {
+    flexDirection: "row",
+    gap: 5,
+    alignItems: "center",
+    paddingTop: 5,
+  },
+  replyInput: {
+    borderColor: "gray",
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 5,
+    marginRight: 5,
+  },
+  input: {
+    backgroundColor: "#FAFAFA",
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    width: "100%",
+  },
+  replyBtn: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    justifyContent: "center",
+    backgroundColor: "#A3DE83",
+  },
+  sky: {
+    height: 600,
+  },
+});
+
+export default RepliesList;
---
+++ app/component/repliemodal.jsx
@@ -0,0 +1,264 @@
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  Image,
+  Modal,
+  StyleSheet,
+  TouchableOpacity,
+  Dimensions,
+  TextInput,
+  Alert,
+  FlatList, // ScrollViewからFlatListに変更z
+  KeyboardAvoidingView,
+  ActivityIndicator,
+} from "react-native";
+import FirebaseAuth from "@react-native-firebase/auth";
+import firestore from "@react-native-firebase/firestore";
+import { formatInTimeZone } from "date-fns-tz";
+import { useRouter } from "expo-router";
+import Toast from "react-native-simple-toast";
+
+const auth = FirebaseAuth();
+const { width, height } = Dimensions.get("window"); //デバイスの幅と高さを取得す
+
+export default function ReplieModal({
+  visible,
+  items,
+  onClose,
+  postId,
+  navigateProfile,
+  parentReplyId,
+  loading,
+}) {
+  const router = useRouter();
+  const [newReplyText, setNewReplyText] = useState(""); // 新しい返信内容
+
+  const submitReply = () => {
+    if (newReplyText.trim()) {
+      onReplySubmit(parentReplyId, newReplyText); // 親返信IDと一緒に返信を送信
+      setNewReplyText("");
+    }
+  };
+
+  const onReplySubmit = async (parentReplyId, newReplyText) => {
+    const currentTime = new Date().toISOString();
+    if (newReplyText.trim()) {
+      if (!auth.currentUser) {
+        Alert.alert("エラー", "ログインしてください。");
+        return;
+      }
+      const userId = auth.currentUser.uid;
+
+      try {
+        await firestore()
+          .collection("replies")
+          .add({
+            postId: parseInt(postId),
+            parentReplyId: parentReplyId,
+            userId: userId,
+            text: newReplyText,
+            timestamp: currentTime,
+            hantei: 1,
+          });
+
+        Toast.show("送信しました");
+        router.back();
+      } catch (error) {
+        Alert.alert(
+          "エラー",
+          `返信の送信中にエラーが発生しました: ${error.message}`
+        );
+        console.error("Error adding reply:", error);
+      }
+    } else {
+      Alert.alert("エラー", "返信を入力してください。");
+    }
+  };
+  const renderReply = ({ item }) => (
+    <View
+      style={[
+        styles.replyContainer,
+        item.hantei === 1 && styles.indentedReplyContainer, // hanteiが1なら全体をインデント
+      ]}
+    >
+      <View style={styles.header}>
+        <TouchableOpacity
+          style={styles.userBar}
+          onPress={() => navigateProfile(item.userId)}
+        >
+          <Image
+            source={{ uri: item.userData.photoURL }}
+            style={styles.iconImage}
+          />
+          <Text style={{ alignItems: "flex-end" }}>
+            {item.userData.displayName}
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text style={styles.replyText}>{item.text}</Text>
+      <Text style={styles.replyTimestamp}>
+        {formatInTimeZone(
+          new Date(item.timestamp),
+          "Asia/Tokyo",
+          "yyyy年MM月dd日 HH:mm"
+        )}
+      </Text>
+    </View>
+  );
+  return (
+    <Modal
+      animationType="fade"
+      transparent={true}
+      visible={visible}
+      onRequestClose={onClose}
+      style={styles.modal}
+    >
+      <KeyboardAvoidingView style={styles.centeredView}>
+        {loading ? (
+          <View style={styles.loadingstyle}>
+            <ActivityIndicator size="large" color="#239D60" />
+            <Text>読み込み中...</Text>
+          </View>
+        ) : (
+          <View style={styles.postViewCentering}>
+            <View style={styles.closeButton}>
+              <TouchableOpacity style={styles.button} onPress={onClose}>
+                <Image
+                  source={require("./../image/Close.png")}
+                  style={styles.actionButton}
+                />
+              </TouchableOpacity>
+            </View>
+            <FlatList
+              data={items} // ソートされた配列を使用
+              renderItem={renderReply}
+              keyExtractor={(item) => item.id}
+              style={styles.repliesList}
+              contentContainerStyle={{ flexGrow: 1 }}
+              ListEmptyComponent={
+                <Text style={styles.noRepliesText}>まだ返信がありません。</Text>
+              }
+            />
+            <View style={styles.replys}>
+              <View style={styles.replyTextinput}>
+                <TextInput
+                  style={styles.input}
+                  value={newReplyText}
+                  onChangeText={setNewReplyText}
+                  placeholder="返信を入力..."
+                />
+                <TouchableOpacity style={styles.replyBtn} onPress={submitReply}>
+                  <Text>送信</Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        )}
+      </KeyboardAvoidingView>
+    </Modal>
+  );
+}
+
+const styles = StyleSheet.create({
+  centeredView: {
+    justifyContent: "center",
+    alignItems: "center",
+    height: height,
+    flex: 1,
+    backgroundColor: "rgba(0, 0, 0, 0.4)",
+  },
+  replys: {
+    width: "100%",
+  },
+  input: {
+    backgroundColor: "#FAFEFF",
+    borderColor: "gray",
+    borderWidth: 1,
+    padding: 5,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    width: "75%",
+  },
+  postViewCentering: {
+    width: 350,
+    padding: 20,
+    backgroundColor: "#F2F5C8",
+    borderRadius: 20,
+    height: height * 0.8,
+    flex: 0.7,
+  },
+  loadingstyle: {
+    width: 350,
+    padding: 20,
+    alignItems: "center",
+    backgroundColor: "#F2F5C8",
+    borderRadius: 20,
+  },
+  iconImage: {
+    width: 26,
+    height: 26,
+    borderRadius: 13,
+  },
+  header: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  userBar: {
+    flexDirection: "row",
+    gap: 8,
+    justifyContent: "center",
+  },
+  replyText: {
+    fontSize: 16,
+    paddingHorizontal: 10,
+  },
+  replyBtn: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    justifyContent: "center",
+    backgroundColor: "#A3DE83",
+  },
+  closeButton: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "flex-end",
+  },
+  actionButton: {
+    width: 30,
+    height: 30,
+    padding: 5,
+    margin: 5,
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center", // ボタン内のテキストを中央に配置
+    alignItems: "center",
+  },
+  replyTextinput: {
+    justifyContent: "space-between",
+    flexDirection: "row", // 横並びに設定
+    gap: 10,
+  },
+  modal: {
+    backgroundColor: "#F0E68C",
+  },
+  replyContainer: {
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: "lightgray",
+  },
+  indentedReplyContainer: {
+    marginLeft: 40, // 4マス分のインデント
+  },
+  repliesList: {
+    height: "80%",
+  },
+  replyTimestamp: {
+    fontSize: 12,
+    color: "gray",
+  },
+});
