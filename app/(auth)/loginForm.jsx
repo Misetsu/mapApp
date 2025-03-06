e0b0c88e55b4055e529008329c64b40d2ca9fb16
syu-to <kd1333767@st.kobedenshi.ac.jp>
2025-03-07 01:50:57 +0900

    諸々の設定

--- app/(auth)/loginForm.jsx
+++ app/(auth)/loginForm.jsx
@@ -14,8 +14,37 @@ import FirebaseAuth from "@react-native-firebase/auth";
 import firestore from "@react-native-firebase/firestore";
 import { GoogleSignin } from "@react-native-google-signin/google-signin";
 import Toast from 'react-native-simple-toast';
+import { appleAuth, AppleButton } from '@invertase/react-native-apple-authentication';
 
 const auth = FirebaseAuth();
+
+async function onAppleButtonPress() {
+  // 1). start a apple sign-in request
+  const appleAuthRequestResponse = await appleAuth.performRequest({
+    requestedOperation: appleAuth.Operation.LOGIN,
+    requestedScopes: [appleAuth.Scope.EMAIL, appleAuth.Scope.FULL_NAME],
+  });
+
+  // 2). if the request was successful, extract the token and nonce
+  const { identityToken, nonce } = appleAuthRequestResponse;
+
+  // can be null in some scenarios
+  if (identityToken) {
+    // 3). create a Firebase `AppleAuthProvider` credential
+    const appleCredential = firebase.auth.AppleAuthProvider.credential(identityToken, nonce);
+
+    // 4). use the created `AppleAuthProvider` credential to start a Firebase auth request,
+    //     in this example `signInWithCredential` is used, but you could also call `linkWithCredential`
+    //     to link the account to an existing user
+    const userCredential = await firebase.auth().signInWithCredential(appleCredential);
+
+    // user is now signed in, any Firebase `onAuthStateChanged` listeners you have will trigger
+    console.warn(`Firebase authenticated via Apple, UID: ${userCredential.user.uid}`);
+  } else {
+    // handle this - retry?
+  }
+}
+
 
 GoogleSignin.configure({
   webClientId:
@@ -151,6 +180,19 @@ export default function LoginScreen() {
             style={styles.imageButton}
           />
         </TouchableOpacity>
+    <View>
+      {/* Render your other social provider buttons here */}
+      {console.log(appleAuth.isSupported)}
+      {appleAuth.isSupported && (
+        <AppleButton
+          cornerRadius={5}
+          style={{ width: 200, height: 60 }}
+          buttonStyle={AppleButton.Style.WHITE}
+          buttonType={AppleButton.Type.SIGN_IN}
+          onPress={() => onAppleButtonPress()}
+        />
+      )}
+    </View>
         <View
           style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
         >
